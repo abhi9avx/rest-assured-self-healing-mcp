45434443cde47fa09621aed548baeb06ace9ee23
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2026-01-11 16:59:29 +0530

    failed the test case

--- rest-api-test-example/src/main/java/com/example/framework/controller/UserController.java
+++ rest-api-test-example/src/main/java/com/example/framework/controller/UserController.java
@@ -7,7 +7,7 @@ import io.restassured.response.Response;
 public class UserController {
 
     public Response getUser(int userId) {
-        String url = ApiConfig.BASE_URL + ApiConfig.USERS_ENDPOINT + "/" + userId;
+        String url = ApiConfig.BASE_URL + ApiConfig.USERS_ENDPOINT + "" + userId;
         return ApiClient.get(url);
     }
 
